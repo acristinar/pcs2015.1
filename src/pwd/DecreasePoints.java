1e506772b4a9fb1906ea872c966946432db6692a
Lucianna <delucianna@ig.com.br>
2015-05-04 23:18:55 -0300

    Revert "editando nome de variáveis e funções"
    This reverts commit 12ccac9bbd5c430d5ea2a1dd6eb41732125cf26a.

--- src/pwd/DecreasePoints.java
+++ src/pwd/DecreasePoints.java
@@ -11,25 +11,25 @@ package pwd;
  */
 public class DecreasePoints extends CountPoints{
 
-    public static int decreaseLevel(String password, int length, int level) {
-        setQuantity(password);
+    public static int decreaseLevel(String passwd, int length, int level) {
+        setQuant(passwd);
         
-        level = simpleSequence(password, level);
+        level = simpleSequence(passwd, level);
 
-        if (getQuantityOfNumber() == length || getQuantityOfSmallLetter() == length || getQuantityOfCapitalLetter() == length) {
+        if (getQuantNum() == length || getQuantSmallLetter() == length || getQuantCapitalLetter() == length) {
             level--;
         }
         if (length % 2 == 0) { // aaabbb
-            level = pairRepeatSequence(length, password, level);
+            level = pairRepeatSequence(length, passwd, level);
         }
         if (length % 3 == 0) { // ababab
-            level = oddRepeatSequence(length, password, level);
+            level = oddRepeatSequence(length, passwd, level);
         }
-        if (StringUtils.isNumeric(password) && length >= 6) { // 19881010 or 881010
-            level = date(password, length, level);
+        if (StringUtils.isNumeric(passwd) && length >= 6) { // 19881010 or 881010
+            level = date(passwd, length, level);
         }
         if (null != getDICTIONARY() && getDICTIONARY().length > 0) {
-            level = wordDictionary(password, level);
+            level = wordDictionary(passwd, level);
         }
         if (length <= 6) {
             level--;
@@ -40,7 +40,7 @@ public class DecreasePoints extends CountPoints{
         if (length <= 3) {
             level = 0;
         }
-        if (StringUtils.isCharEqual(password)) {
+        if (StringUtils.isCharEqual(passwd)) {
             level = 0;
         }
         if (level < 0) {
@@ -52,9 +52,9 @@ public class DecreasePoints extends CountPoints{
     /**
      * Compare password with words from the dictionary
      */
-    private static int wordDictionary(String password, int level) {
+    private static int wordDictionary(String passwd, int level) {
         for (int i = 0; i < getDICTIONARY().length; i++) {
-            if (password.equals(getDICTIONARY()[i]) || getDICTIONARY()[i].indexOf(password) >= 0) {
+            if (passwd.equals(getDICTIONARY()[i]) || getDICTIONARY()[i].indexOf(passwd) >= 0) {
                 level--;
                 break;
             }
@@ -65,14 +65,14 @@ public class DecreasePoints extends CountPoints{
     /**
      * Judge whether the password is a date
      */
-    private static int date(String password, int length, int level) throws NumberFormatException {
+    private static int date(String passwd, int length, int level) throws NumberFormatException {
         int year = 0;
         if (length == 8 || length == 6) {
-            year = Integer.parseInt(password.substring(0, length - 4));
+            year = Integer.parseInt(passwd.substring(0, length - 4));
         }
         int size = StringUtils.sizeOfInt(year);
-        int month = Integer.parseInt(password.substring(size, size + 2));
-        int day = Integer.parseInt(password.substring(size + 2, length));
+        int month = Integer.parseInt(passwd.substring(size, size + 2));
+        int day = Integer.parseInt(passwd.substring(size + 2, length));
         if (year >= 1950 && year < 2050 && month >= 1 && month <= 12 && day >= 1 && day <= 31) {
             level--;
         }
@@ -82,10 +82,10 @@ public class DecreasePoints extends CountPoints{
     /**
      * Judge whether the password is an odd repeat sequence
      */
-    private static int oddRepeatSequence(int length, String password, int level) {
-        String part1 = password.substring(0, length / 3);
-        String part2 = password.substring(length / 3, length / 3 * 2);
-        String part3 = password.substring(length / 3 * 2);
+    private static int oddRepeatSequence(int length, String passwd, int level) {
+        String part1 = passwd.substring(0, length / 3);
+        String part2 = passwd.substring(length / 3, length / 3 * 2);
+        String part3 = passwd.substring(length / 3 * 2);
         if (part1.equals(part2) && part2.equals(part3)) {
             level--;
         }
@@ -95,9 +95,9 @@ public class DecreasePoints extends CountPoints{
     /**
      * Judge whether the password is a pair repeat sequence
      */
-    private static int pairRepeatSequence(int length, String password, int level) {
-        String part1 = password.substring(0, length / 2);
-        String part2 = password.substring(length / 2);
+    private static int pairRepeatSequence(int length, String passwd, int level) {
+        String part1 = passwd.substring(0, length / 2);
+        String part2 = passwd.substring(length / 2);
         if (part1.equals(part2)) {
             level--;
         }
@@ -111,14 +111,14 @@ public class DecreasePoints extends CountPoints{
     /**
      * Judge whether the password is a common sequence
      */
-    private static int simpleSequence(String password, int level) {
-        if ("abcdefghijklmnopqrstuvwxyz".indexOf(password) > 0 || "ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(password) > 0) {
+    private static int simpleSequence(String passwd, int level) {
+        if ("abcdefghijklmnopqrstuvwxyz".indexOf(passwd) > 0 || "ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(passwd) > 0) {
             level--;
         }
-        if ("qwertyuiop".indexOf(password) > 0 || "asdfghjkl".indexOf(password) > 0 || "zxcvbnm".indexOf(password) > 0) {
+        if ("qwertyuiop".indexOf(passwd) > 0 || "asdfghjkl".indexOf(passwd) > 0 || "zxcvbnm".indexOf(passwd) > 0) {
             level--;
         }
-        if (StringUtils.isNumeric(password) && ("01234567890".indexOf(password) > 0 || "09876543210".indexOf(password) > 0)) {
+        if (StringUtils.isNumeric(passwd) && ("01234567890".indexOf(passwd) > 0 || "09876543210".indexOf(passwd) > 0)) {
             level--;
         }
         return level;
