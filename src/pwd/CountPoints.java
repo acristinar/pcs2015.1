cab8c8dd23157cfa06b2d15a81f189f28c2c3a98
Lucianna <delucianna@ig.com.br>
2015-05-04 22:38:15 -0300

    editando nome de variáveis e funções

--- src/pwd/CountPoints.java
+++ src/pwd/CountPoints.java
@@ -13,37 +13,37 @@ import java.util.Map;
  * @author Lucianna
  */
 public class CountPoints {
-    private static int quantNum;
-    private static int quantSmallLetter;
-    private static int quantCapitalLetter;
-    private static int quantOtherChar;
+    private static int quantityOfNumber;
+    private static int quantityOfSmallLetter;
+    private static int quantityOfCapitalLetter;
+    private static int quantityOfOtherChar;
     private static Map<String, Integer> letter = new HashMap<>();
     
     private final static String[] DICTIONARY = {"password", "abc123", "iloveyou", "adobe123", "123123", "sunshine",
         "1314520", "a1b2c3", "123qwe", "aaa111", "qweasd", "admin", "passwd"};
 
-    public static void setQuant(String passwd) {
-        letter = StringUtils.quantLetter(passwd);
-        quantNum = letter.get("NUM");
-        quantSmallLetter = letter.get("SMALL_LETTER");
-        quantCapitalLetter = letter.get("CAPITAL_LETTER");
-        quantOtherChar = letter.get("OTHER_CHAR");
+    public static void setQuantity(String password) {
+        letter = StringUtils.quantityOfLetter(password);
+        quantityOfNumber = letter.get("NUMBER");
+        quantityOfSmallLetter = letter.get("SMALL_LETTER");
+        quantityOfCapitalLetter = letter.get("CAPITAL_LETTER");
+        quantityOfOtherChar = letter.get("OTHER_CHAR");
     }
 
-    public static int getQuantNum() {
-        return quantNum;
+    public static int getQuantityOfNumber() {
+        return quantityOfNumber;
     }
 
-    public static int getQuantSmallLetter() {
-        return quantSmallLetter;
+    public static int getQuantityOfSmallLetter() {
+        return quantityOfSmallLetter;
     }
 
-    public static int getQuantCapitalLetter() {
-        return quantCapitalLetter;
+    public static int getQuantityOfCapitalLetter() {
+        return quantityOfCapitalLetter;
     }
 
-    public static int getQuantOtherChar() {
-        return quantOtherChar;
+    public static int getQuantityOfOtherChar() {
+        return quantityOfOtherChar;
     }
 
     public static String[] getDICTIONARY() {
